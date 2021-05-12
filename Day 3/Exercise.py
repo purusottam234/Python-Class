7cd5e2df031368ab9785c97ae422b5cbd0e416a3
Purusottam <purusottamadhikari234@gmail.com>
2021-05-12 10:47:53 +0545

    day 3

---
+++ Day 3/Exercise.py
@@ -0,0 +1,10 @@
+# Program to demonstrate the arithmatic operator with right operand 25.5 and left operand 2
+a = 25.5
+b = 2
+print("addition is: ", a+b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print("subtraction:", a-b)
+print("floor division:", a//b)
+print("modulo division:", a % b)
+print("exponential:", a ** b)
---
+++ Day 3/arithmetic.py
@@ -0,0 +1,33 @@
+# multiplication(*)
+
+a = 7*4
+print(a)
+
+# exponentiation(**) raises one value to power of another value
+b = 2**2
+print(b)
+# Division vs floor division
+# floor division removes fractional part and less than the normal division.
+
+c = 7/4
+print(c)
+d = 7//4
+print(d)
+f = -15/4
+print(f)
+e = -15//4
+print(e)
+
+# Exception
+# 1. dividing  by zero(0)
+
+# z = 20/0
+# print(z)
+
+# modulo operator returns remainder
+y = 16 % 5
+print(y)
+
+# grouping operator
+m = 8*4+(5*6)-5
+print(m)
