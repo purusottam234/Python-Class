522c96efb447d91ae82c5727ae836eac29e868af
Purusottam <purusottamadhikari234@gmail.com>
2021-05-14 21:11:18 +0545

    Exercise

--- Day 5/exercise.py
+++ Day 5/exercise.py
@@ -1,4 +1,41 @@
-1. Show that a what occurs if ...elif statement specifies an else before the last elif.
-2. program to implement the if statement
+# 1. Show that a what occurs if ...elif statement specifies an else before the last elif.
+
+grade = 80
+if grade >= 90:
+    print("A")
+
+elif grade >= 80:
+    print("B")
+
+else:
+    print("fail")
+
+
+elif grade >= 70:
+    print("c")
+
+# 2. program to implement the if statement
+
+grade = 10
+if grade >= 40:
+    print("pass")
+
 3. program to implement the if else statement
+age = 20
+if age >= 20:
+    print("they are ready to marriage")
+else:
+    print("you belong to young")
+
 4. program to implement the if ..elif statement
+age = int(input("Enter your age:"))
+if age >= 13:
+    print("teen")
+
+elif age >= 20:
+    print('young')
+elif age >= 50:
+    print('ols')
+
+else:
+    print("child")
