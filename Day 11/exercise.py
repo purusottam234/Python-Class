e2e89f4dda0bd261a47be5fcc24103fee9be75d8
Purusottam <purusottamadhikari234@gmail.com>
2021-05-20 20:02:10 +0545

    Day 11

---
+++ Day 11/exercise.py
@@ -0,0 +1,1 @@
+# Program to implement different math module at least 8.
---
+++ Day 11/mathmodule.py
@@ -0,0 +1,13 @@
+# math module defines functions for performing various common mathematical calculation
+
+import math
+a = math.sqrt(900)
+print(a)
+
+b = math.fabs(-10)
+print(b)
+
+c = math.floor(9.2)
+print(c)
+print(math.cos(0))
+print(math.pow(2, 7))
